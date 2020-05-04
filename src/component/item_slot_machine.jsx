0b1e43ed3fe7b45e9f52826563b16852e2864f42
vusown <vuson2497@gmail.com>
2020-05-05 06:21:30 +0700

    done

--- src/component/item_slot_machine.jsx
+++ src/component/item_slot_machine.jsx
@@ -4,15 +4,18 @@ class ItemSlotMachine extends Component {
 
 	constructor(props) {
 		super(props);
+		
 		var imgSet = [
-			'../assets/img/build-64',
-			'../assets/img/cash-64',
-			'../assets/img/energy-64',
-			'../assets/img/gold-64',
-			'../assets/img/goods-64',
-			'../assets/img/staff-64'
+			{url: 'http://ikonen.me/examples/slot/img/build-64.png'},
+			{url: 'http://ikonen.me/examples/slot/img/cash-64.png'},
+			{url: 'http://ikonen.me/examples/slot/img/energy-64.png'},
+			{url: 'http://ikonen.me/examples/slot/img/gold-64.png'},
+			{url: 'http://ikonen.me/examples/slot/img/goods-64.png'},
+			{url: 'http://ikonen.me/examples/slot/img/staff-64.png'},
+
 		]
 		imgSet = this.shuffleArray(imgSet)
+		
 		this.state = {
 			'urlimage': imgSet,
 			'define': {
@@ -27,8 +30,8 @@ class ItemSlotMachine extends Component {
 				'SLOT_SPEED': 15, // how many pixels per second slots roll
 				'DRAW_OFFSET': 45, // how much draw offset in slot display from top
 			}
-
 		}
+		this.canvas = React.createRef();
 	}
 
 	shuffleArray(array) {
@@ -41,57 +44,10 @@ class ItemSlotMachine extends Component {
 		return array
 	}
 
-	loadImages(arr, callback) {
-		this.images = {};
-		var loadedImageCount = 0;
-
-		// Make sure arr is actually an array and any other error checking
-		for (var i = 0; i < arr.length; i++){
-			var img = new Image();
-			img.onload = imageLoaded;
-			img.src = arr[i];
-			this.images[arr[i] = img;
-		}
-
-		function imageLoaded(e) {
-			loadedImageCount++;
-			if (loadedImageCount >= arr.length) {
-				callback();
-			}
-		}
-	}
-
-	preloadImage() {
-		const canvas = this.refs.canvas
-		var ctx = canvas.getContext('2d');
-		ctx.fillStyle = '#ddd';
-
-		for (var i = 0 ; i < this.state.define.ITEM_COUNT ; i++) {
-			let img = new Image()
-			img.src = 'https://images.viblo.asia/avatar/3baf3941-2d6b-4a27-b8e1-a6654609098f.jpg';
-			img.addEventListener("load", function() {
-
-			}, false);
-			var asset = this.state.urlimage[i];
-			console.log(asset)
-			ctx.save();
-			ctx.shadowColor = "rgba(0,0,0,0.5)";
-			ctx.shadowOffsetX = 5;
-			ctx.shadowOffsetY = 5;
-			ctx.shadowBlur = 5;
-			ctx.drawImage(img, (98 - this.state.define.IMAGE_HEIGHT)/2, i * this.state.define.SLOT_HEIGHT + this.state.define.IMAGE_TOP_MARGIN, 32, 32);
-			//ctx.drawImage(img.src, (98 - this.state.define.IMAGE_HEIGHT)/2, (i + this.state.define.ITEM_COUNT) * this.state.define.SLOT_HEIGHT + this.state.define.IMAGE_TOP_MARGIN, 32, 32);
-			ctx.restore();
-			ctx.fillRect(0, i * this.state.define.SLOT_HEIGHT, 98, this.state.define.SLOT_SEPARATOR_HEIGHT);
-			ctx.fillRect(0, (i + this.state.define.ITEM_COUNT)  * this.state.define.SLOT_HEIGHT, 98, this.state.define.SLOT_SEPARATOR_HEIGHT);
-		}
-	}
-
 	preloadImages( images, callback ) {
-
 		function _preload( asset ) {
 			asset.img = new Image();
-			asset.img.src = 'https://images.viblo.asia/avatar/3baf3941-2d6b-4a27-b8e1-a6654609098f.jpg';
+			asset.img.src = asset.url;
 
 			asset.img.addEventListener("load", function() {
 				_check();
@@ -117,39 +73,54 @@ class ItemSlotMachine extends Component {
 		});
 	}
 
-
-
-	componentDidMount() {
-		preloadImages( this.state.urlimage, function() {
+	drawPreloadImages() {
+		this.preloadImages( this.state.urlimage, () => {
 			function _fill_canvas( canvas, items ) {
-				const canvas = this.refs.canvas
 				var ctx = canvas.getContext('2d');
 				ctx.fillStyle = '#ddd';
-
-				for (var i = 0 ; i < ITEM_COUNT ; i++) {
+				
+				for (var i = 0 ; i < this.state.define.ITEM_COUNT ; i++) {
 					var asset = items[i];
 					ctx.save();
 					ctx.shadowColor = "rgba(0,0,0,0.5)";
 					ctx.shadowOffsetX = 5;
 					ctx.shadowOffsetY = 5;
 					ctx.shadowBlur = 5;
-					ctx.drawImage(asset.img, (98 - IMAGE_HEIGHT)/2, i * SLOT_HEIGHT + IMAGE_TOP_MARGIN, 32, 32);
-					ctx.drawImage(asset.img, (98 - IMAGE_HEIGHT)/2, (i + ITEM_COUNT) * SLOT_HEIGHT + IMAGE_TOP_MARGIN, 32, 32);
+					ctx.drawImage(asset.img, (98 - this.state.define.IMAGE_HEIGHT)/2, i * this.state.define.SLOT_HEIGHT + this.state.define.IMAGE_TOP_MARGIN, 32, 32);
+					ctx.drawImage(asset.img, (98 - this.state.define.IMAGE_HEIGHT)/2, (i + this.state.define.ITEM_COUNT) * this.state.define.SLOT_HEIGHT + this.state.define.IMAGE_TOP_MARGIN, 32, 32);
 					ctx.restore();
-					ctx.fillRect(0, i * SLOT_HEIGHT, 98, SLOT_SEPARATOR_HEIGHT);
-					ctx.fillRect(0, (i + ITEM_COUNT)  * SLOT_HEIGHT, 98, SLOT_SEPARATOR_HEIGHT);
+					ctx.fillRect(0, i * this.state.define.SLOT_HEIGHT, 98, this.state.define.SLOT_SEPARATOR_HEIGHT);
+					ctx.fillRect(0, (i + this.state.define.ITEM_COUNT)  * this.state.define.SLOT_HEIGHT, 98, this.state.define.SLOT_SEPARATOR_HEIGHT);
 				}
 			}
+			_fill_canvas.bind(this)( this.canvas.current, this.state.urlimage );
+			this.canvas.current.style.height = (this.state.define.IMAGE_HEIGHT * this.state.define.ITEM_COUNT * 2) + 'px'
 		})
+	}
+	
+	spin() {
+		let currentPx = 0;
+    
+    // setInterval(() => {
+		// 	this.canvas.current.style.transform = 'translate3d(0px, '+ (currentPx -= 1) +'px, 0px)'
+		// }, 1000);
+		this.canvas.current.style.transform = 'translate3d(0px,-' +4*this.state.define.SLOT_HEIGHT+'px, 0px)'
+	}
+	
+	componentDidMount() {
+		this.drawPreloadImages.bind(this)()
+		if(true) {
+			this.spin.bind(this)()
+		}
 	}
 
 	render() {
 		return(
 			<canvas
 				id="canvas1"
-				width={100}
-				height={this.state.define.IMAGE_HEIGHT * this.state.define.ITEM_COUNT * 2}
-				ref="canvas"
+				width={'100px'}
+				height={this.state.define.IMAGE_HEIGHT * this.state.define.ITEM_COUNT*2}
+				ref={this.canvas}
 			></canvas>
 		);
 	}
--- src/component/slot_machine.jsx
+++ src/component/slot_machine.jsx
@@ -11,6 +11,20 @@ class SlotMachine extends Component {
 
 	}
 
+	
+	componentDidMount() {
+		window.requestAnimFrame = (function(){
+			return window.requestAnimationFrame ||
+				window.webkitRequestAnimationFrame ||
+				window.mozRequestAnimationFrame ||
+				window.oRequestAnimationFrame ||
+				window.msRequestAnimationFrame ||
+				function(/* function */ callback, /* DOMElement */ element){
+					window.setTimeout(callback, 1000 / 60);
+				};
+		})();
+	}
+	
 	render() {
 		return(
 			<div id="viewport">
